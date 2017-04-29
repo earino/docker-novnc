f90b552f1957acb10e1426e20764dd003deefedd
E. Ariño de la Rubia <earino@gmail.com>
2017-04-29 12:25:47 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@ Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 xfce4-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:8888
+cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:8888 --listen 8888
 while :
 do
   sleep 1
